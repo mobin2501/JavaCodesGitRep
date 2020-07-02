f034f8286d83f0f1c736023d6c345b839787e182
Mobin Rahman <mobin2501@gmail.com>
2020-07-03 05:39:53 +0600

    42

--- EularProject/src/euProFrom26/CodedTriangleNumber.java
+++ EularProject/src/euProFrom26/CodedTriangleNumber.java
@@ -7,6 +7,9 @@ import java.io.FileNotFoundException;
 public class CodedTriangleNumber {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		int count=0;
+		//System.out.println((int)'A'-64);
+		System.out.println(check("SKY"));
 		File f=new File("H:\\Eular\\p042_words.txt");
 		ArrayList<String> sal=new ArrayList<String>();
 		try{
@@ -15,12 +18,37 @@ public class CodedTriangleNumber {
 			while(sc.hasNext()) {
 				sal.add(sc.next());
 			}
-			System.out.print(sal.get(sal.size()-1));
 			sc.close();
 		}
 		catch(FileNotFoundException e) {
 			System.out.print("not found");
 		}
+		for(int i=0;i<sal.size();i++) {
+			if(check(sal.get(i))==true){
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+	static boolean check(String str) {
+		char[] c=str.toCharArray();
+		int t=0;
+		for(int j=0;j<c.length;j++) {
+			//System.out.print(c[j]+" ");
+			//System.out.print((int)c[j]-64);
+			//System.out.println();
+			t=t+((int)c[j]-64);
+		}
+		//System.out.println(t);
 		
+		double a=-.5+(.5*Math.sqrt(1+(8*t)));
+		
+		//System.out.print(a);
+		if(a>0 && Math.ceil(a)-a==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
