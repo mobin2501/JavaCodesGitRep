c62a731ca4d48a96d9f92d1be4241906c600b77a
Mobin Rahman <31849645+mobin2501@users.noreply.github.com>
2020-07-18 22:20:19 +0600

    41

--- EularProject2/src/eularThird/IntegerRightTriangle.java
+++ EularProject2/src/eularThird/IntegerRightTriangle.java
@@ -2,7 +2,32 @@ package eularThird;
 
 public class IntegerRightTriangle {
 	public static void main(String[] args) {
-		System.out.println("Bojjfsdo");
+		int max=0;
+		int val=0;
+		int[] p=new int[1000];
+		for(int i=0;i<1000;i++) {
+			p[i]=0;
+		}
+		for(int c=1;c<500;c++) {
+			for(int a=1;a<c;a++) {
+				for(int b=1;b<c;b++) {
+					if(a*a+b*b==c*c && a+b+c<=1000) {
+						if(a+b+c==840) {
+							System.out.println(a+" "+b+" "+c);
+						}
+						
+						p[a+b+c-1]=p[a+b+c-1]+1;
+					}
+				}
+			}
+		}
+		for(int i=0;i<1000;i++) {
+			if(max<p[i]) {
+				max=p[i];
+				val=i+1;
+			}
+		}
+		System.out.println(val);
+		
 	}
-
 }
